1e4e03ff2d8bb936e653424ece69164363f112b2
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-15 00:19:26 -0200

    Removing unused component

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,9 @@
 import Colors from './Colors';
 import ContainerFlexStyled from './ContainerFlexStyled';
-import Feedback from './FeedbackStyled';
 import Letters from './Letters/Letters';
-import Letter from './Letter/Letter';
 
 export {
     Colors,
     ContainerFlexStyled,
-    Feedback,
     Letters,
-    Letter,
 };
