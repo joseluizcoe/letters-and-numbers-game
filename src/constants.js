15a29cb669f81bdce42e5427c6c218186280363a
José Luiz Coe <joseluizcoe@gmail.com>
2020-12-07 20:56:19 -0300

    Using all letters.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const LETTERS = ['A', 'E', 'I', 'O', 'U'];
+// export const LETTERS = ['A', 'E', 'I', 'O', 'U'];
+export const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 export const COLORS = {
   red: 'vermelho',
   green: 'verde',
